b1cacbabc361c94ebf79637a1f0d9d97eabc48ca
Eric Murphy <murphye@gmail.com>
2021-01-22 10:21:26 -0800

    Initial Commit
    About to generate the JAXB code under target

---
+++ address-validation-service/src/main/java/io/solo/addressvalidation/AddressValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package io.solo.addressvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressValidationServiceApplication.class, args);
+	}
+
+}
---
+++ address-validation-service/src/test/java/io/solo/addressvalidation/AddressValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.solo.addressvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
