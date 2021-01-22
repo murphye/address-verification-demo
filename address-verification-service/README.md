c0a95dd8d00ed4eba024247a5594ab1a5232af5f
Eric Murphy <murphye@gmail.com>
2021-01-22 11:39:22 -0800

    Working with hard-coded values

--- address-verification-service/README.md
+++ address-verification-service/README.md
@@ -2,7 +2,13 @@ Address Validation Service
 
 # Build
 
-`mvn clean install`
+`./mvnw clean install`
 
 Generated clases will be created under `target/classes/com/qas/ondemand_2011_03` from the `src/main/resources/qas-ondemand.wsdl`.
+
+# Run
+`./mvnw spring-boot:run`
+
+Curl the Service
+`curl --header "content-type: text/xml" -d @request.xml http://localhost:8080/ws`
 
--- address-verification-service/src/main/java/io/solo/addressverification/QASearchEndpoint.java
+++ address-verification-service/src/main/java/io/solo/addressverification/QASearchEndpoint.java
@@ -17,9 +17,13 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class QASearchEndpoint {
 
-    @PayloadRoot(namespace = "http://www.qas.com/web-2013-12", localPart = "QASearch")
+    @PayloadRoot(namespace = "http://www.qas.com/OnDemand-2011-03", localPart = "QASearch")
 	@ResponsePayload
 	public QASearchResult verifyAddress(@RequestPayload QASearch request) {
+
+		String searchString = request.getSearch();
+		// TODO: Parse the searchString to get some valid values
+
 		QASearchResult result = new QASearchResult();
 
 		result.setVerifyLevel(VerifyLevelType.VERIFIED);
@@ -30,10 +34,72 @@ public class QASearchEndpoint {
 		QAAddressType qaAddress = new QAAddressType();
 		qaAddress.setDPVStatus(DPVStatusType.DPV_CONFIRMED);
 
-		AddressLineType addressLine = new AddressLineType();
-		addressLine.setLineContent(LineContentType.ADDRESS);
-		addressLine.setLabel("Premise");
-		addressLine.setLine("ABC 123");
+		AddressLineType addressLine1 = new AddressLineType();
+		addressLine1.setLineContent(LineContentType.ADDRESS);
+		addressLine1.setLabel("Premise");
+		addressLine1.setLine("ABC 123");
+
+		AddressLineType addressLine2 = new AddressLineType();
+		addressLine2.setLineContent(LineContentType.ADDRESS);
+		addressLine2.setLabel("City name");
+		addressLine2.setLine("ABCD");
+
+		AddressLineType addressLine3 = new AddressLineType();
+		addressLine3.setLineContent(LineContentType.ADDRESS);
+		addressLine3.setLabel("State code");
+		addressLine3.setLine("AB");
+
+		AddressLineType addressLine4 = new AddressLineType();
+		addressLine4.setLineContent(LineContentType.ADDRESS);
+		addressLine4.setLabel("Zip-Code");
+		addressLine4.setLine("12345-9876");
+
+		AddressLineType addressLine5 = new AddressLineType();
+		addressLine5.setLineContent(LineContentType.ADDRESS);
+		addressLine5.setLabel("Country");
+		addressLine5.setLine("UNITED STATES OF AMERICA");
+
+		AddressLineType addressLine6 = new AddressLineType();
+		addressLine6.setLineContent(LineContentType.DATA_PLUS);
+		addressLine6.setLabel("PBSA");
+		addressLine6.setLine("N");
+
+		AddressLineType addressLine7 = new AddressLineType();
+		addressLine7.setLineContent(LineContentType.DATA_PLUS);
+		addressLine7.setLabel("CMRA");
+		addressLine7.setLine("N");
+
+		AddressLineType addressLine8 = new AddressLineType();
+		addressLine8.setLineContent(LineContentType.DATA_PLUS);
+		addressLine8.setLabel("ZIP5");
+		addressLine8.setLine("0");
+
+		AddressLineType addressLine9 = new AddressLineType();
+		addressLine9.setLineContent(LineContentType.DATA_PLUS);
+		addressLine9.setLabel("ZIP4");
+		addressLine9.setLine("1");
+
+		AddressLineType addressLine10 = new AddressLineType();
+		addressLine10.setLineContent(LineContentType.DATA_PLUS);
+		addressLine10.setLabel("RTYP");
+		addressLine10.setLine("S");
+
+		AddressLineType addressLine11 = new AddressLineType();
+		addressLine11.setLineContent(LineContentType.DATA_PLUS);
+		addressLine11.setLabel("Postnet barcode");
+		addressLine11.setLine("138");
+
+		qaAddress.getAddressLine().add(addressLine1);
+		qaAddress.getAddressLine().add(addressLine2);
+		qaAddress.getAddressLine().add(addressLine3);
+		qaAddress.getAddressLine().add(addressLine4);
+		qaAddress.getAddressLine().add(addressLine5);
+		qaAddress.getAddressLine().add(addressLine6);
+		qaAddress.getAddressLine().add(addressLine7);
+		qaAddress.getAddressLine().add(addressLine8);
+		qaAddress.getAddressLine().add(addressLine9);
+		qaAddress.getAddressLine().add(addressLine10);
+		qaAddress.getAddressLine().add(addressLine11);
 
 		result.setQAAddress(qaAddress);
 
